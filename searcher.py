2c24d2d5e990d2c268ba07a8d5758f1331db2464
happy <middlet@gmail.com>
2013-02-09 10:27:27 +0000

    added support for randomised user agent

--- searcher.py
+++ searcher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import config ### replace this with a file containing your accountkey
+import random
 import re
 import requests
 import sys
@@ -42,11 +43,22 @@ def search_bing_web(terms):
 	
 	probably should change the content header so we dont look like a bot
 	"""
+	user_agents = [
+		'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5',
+		'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+		'Mozilla/5.0 (compatible; Konqueror/4.3; Linux 2.6.31-16-generic; X11) KHTML/4.3.2 (like Gecko)',
+		'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9a3pre) Gecko/20070330',
+		'Opera/9.80 (X11; Linux x86_64; U; fr) Presto/2.9.168 Version/11.50',
+		'Mozilla/5.0 (X11; U; Linux x86_64; en-us) AppleWebKit/531.2+ (KHTML, like Gecko) Version/5.0 Safari/531.2+',
+		]
+	
 	sterms = terms.replace(' ', '+')
 	bing_url = 'http://www.bing.com/news/search'
 	search_url = '%s?q=%s' % (bing_url, sterms)
 	# get the data
-	res = requests.get(search_url)
+	#headers = {}
+	#headers['User-Agent'] = random.choice(user_agents)
+	res = requests.get(search_url)#, headers=headers)
 	data = res.content
 	url_list = []
 	for ri in pat.finditer(data):
@@ -58,8 +70,10 @@ def search_bing_web(terms):
 
 if __name__ =='__main__':
 	term = 'today'
-	ul1 = search_bing_api(term)
-	ul2 = search_bing_web(term)
+	for i in range(10):
+		ul1 = search_bing_api(term)
+		ul2 = search_bing_web(term)
+		print len(ul1), len(ul2)
 	
 	
 	
